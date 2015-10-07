5ab9653b955ab734b793411d646234d4ff41c656
Nikhil Jain <njai45@BTPNJAI4557795.sapient.com>
2015-10-07 18:46:10 +0530

    adding styles and code refractoring

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var app = express();
 
-app.use('/', express.static(__dirname + '/htmls'));
+app.use('/', express.static(__dirname + '/public'));
 
 var server = app.listen(3000, function() {
 
     var port = server.address().port;
 
     app.get('/', function(req, res) {
-       res.sendFile('htmls/getUser.html', {root: __dirname })
+       res.sendFile('public/getUser.html', {root: __dirname })
     });
 
     console.log('Example app listening at http://localhost:'+port);
